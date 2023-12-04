863cc330f7c043f953f5d222ac6d5cee9d4022b7
Denis Frolov <skittles940@gmail.com>
2023-12-04 15:48:23 +0300

    fixes

--- 2_crawler_extra_2021/crawler.go
+++ 2_crawler_extra_2021/crawler.go
@@ -35,15 +35,18 @@ type Parser struct {
 	mu           sync.Mutex
 	client       *http.Client
 	fw           map[string]*FileWriter
-	sites        []*Site
+	sitesChan    chan *Site
 	meg          multierror.Group
 	checkCounter uint32
+	workers      uint16
+	Stop         chan struct{}
 }
 
-func NewParser(timeout time.Duration, insecure bool) *Parser {
+func NewParser(timeout time.Duration, workers uint16, insecure bool) *Parser {
 	return &Parser{
-		sites: make([]*Site, 0),
-		fw:    make(map[string]*FileWriter),
+		sitesChan: make(chan *Site, 100),
+		fw:        make(map[string]*FileWriter),
+		workers:   workers,
 		client: &http.Client{
 			Timeout: timeout,
 			Transport: &http.Transport{
@@ -52,6 +55,7 @@ func NewParser(timeout time.Duration, insecure bool) *Parser {
 				},
 			},
 		},
+		Stop: make(chan struct{}),
 	}
 }
 
@@ -71,40 +75,48 @@ func NewFileWriter(filename string) (*FileWriter, error) {
 
 func (p *Parser) LoadSitesFromFile(filepath string) error {
 	file, err := os.Open(filepath)
+	wg := &sync.WaitGroup{}
 	if err != nil {
 		return err
 	}
 	defer file.Close()
-
 	decoder := json.NewDecoder(file)
 	for decoder.More() {
+		wg.Add(1)
 		var site *Site
 		err = decoder.Decode(&site)
 		if err != nil {
 			return err
 		}
-		p.sites = append(p.sites, site)
+		go func(site *Site) {
+			defer wg.Done()
+			p.sitesChan <- site
+		}(site)
 	}
-
-	log.Printf("Loaded %d sites\n", len(p.sites))
+	go func() {
+		wg.Wait()
+		close(p.sitesChan)
+	}()
 	return nil
 }
 
 func (p *Parser) PrintStatus() {
-	ticker := time.NewTicker(1 * time.Second)
-	defer ticker.Stop()
-	for {
-		select {
-		case <-ticker.C:
-			log.Printf("Checked %d sites of %d\n", atomic.LoadUint32(&p.checkCounter), len(p.sites))
-		}
-	}
+	log.Printf("Checked %d sites\n", atomic.LoadUint32(&p.checkCounter))
+}
+
+func (p *Parser) Start() {
+	go p.CheckSites()
 }
 
 func (p *Parser) CheckSites() {
-	for _, site := range p.sites {
+	semaphore := make(chan struct{}, p.workers)
+	for site := range p.sitesChan {
+		semaphore <- struct{}{}
 		site := site
 		p.meg.Go(func() error {
+			defer func() {
+				<-semaphore
+			}()
 			req, err := http.NewRequest(http.MethodGet, site.Url, nil)
 			if err != nil {
 				return err
@@ -167,14 +179,25 @@ func (p *Parser) CheckSites() {
 			log.Printf(err.Error())
 		}
 	}
+
+	close(p.Stop)
 }
 
 func main() {
-	parser := NewParser(30*time.Second, true)
+	parser := NewParser(10*time.Second, 30, true)
+	ticker := time.NewTicker(1 * time.Second)
 	err := parser.LoadSitesFromFile("./500.jsonl")
 	if err != nil {
 		panic(err)
 	}
-	go parser.PrintStatus()
-	parser.CheckSites()
+	parser.Start()
+
+	for {
+		select {
+		case <-parser.Stop:
+			return
+		case <-ticker.C:
+			parser.PrintStatus()
+		}
+	}
 }
